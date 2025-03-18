f6aff1e6d00c0216a5bba107184f63b14c016008
FilipEkman <filip.ekman@capgemini.com>
2025-03-18 15:01:27 +0000

    added variables and changed incremental load

---
+++ dbtlearn/macros/logging.sql
@@ -0,0 +1,3 @@
+{% macro learn_logging() %}
+    {# log("Call you dad!", info = true) #}
+{% endmacro %}
--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -11,6 +11,14 @@ select
 {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
  * from src_reviews
 where review_text is not null
+
 {% if is_incremental() %}
-and review_date > (select max(review_date) from {{ this }})
+    {% if var("start_date", false) and var("end_date", false) %}
+        {{ log('Loading ' ~ this ~ ' incementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info = true) }}
+        and review_date >= '{{ var("start_date") }}'
+        and review_date < '{{ var("end_date") }}'
+    {% else %}
+        and review_date > (select max(review_date) from {{ this }})
+        {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info = true) }}
+    {% endif %}
 {% endif %}
